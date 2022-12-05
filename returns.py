657a5cf57de289f4500febb7be94ea44fff73508
atanasije lalkov <atanasijelalkov@gmail.com>
2022-12-05 22:52:18 +0000

    Implmented code

--- returns.py
+++ returns.py
@@ -1,27 +1,23 @@
 import pandas as pd
 import numpy as np
 
-def log_returns(df, T):
-    """
-    Function to compute log returns for different sampling frequencies
-    """
-    # Assign stock price for selected ticker
-    stock_price = df
+def log_returns(df, ticker, T):
+    # Assign stock price for chosen stock
+    stock_price = df[ticker]
 
-    match T:
-        # If conditional statement if sampling frequency is Daily
-        case 'D':
-            # Compute log-returns
-            returns = np.diff(np.log(stock_price))
-        # Normal case where sampling is annual or monthly
-        case _:
-            # Resample the stock price for the corresponding stock
-            stock_price = stock_price.resample(T).last()
-            # Compute log-returns
-            returns = np.diff(np.log(stock_price))
+    # If conditional statement if sampling frequency is Daily
+    if T == 'D':
+        # Compute log-returns
+        returns = np.diff(np.log(stock_price))
+    # Conditional statement for sampling frequencies that are Monthly and Annual
+    else:
+        # Resample the stock price for the corresponding stock
+        stock_price = stock_price.resample(T).last()
+        # Compute log-returns
+        returns = np.diff(np.log(stock_price))
     
     ## Assign returns of stock to DataFrame
-    returns = pd.DataFrame({"Stock": returns}, index = stock_price.index[1:])
+    returns = pd.DataFrame({ticker: returns}, index = stock_price.index[1:])
 
     # Return the returns of the stock
     return(returns)
